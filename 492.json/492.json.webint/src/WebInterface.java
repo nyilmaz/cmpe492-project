5b58f7e801a0a264b9ca5378f31d66b3c7a9a004
nyilmaz <nyilmaz88@gmail.com>
2012-10-03 22:09:39 +0300

    webint, calisan ilk ornek.

--- 492.json/492.json.webint/src/WebInterface.java
+++ 492.json/492.json.webint/src/WebInterface.java
@@ -1,4 +1,17 @@
-import java.io.InputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: nyilmaz
@@ -18,6 +31,38 @@ public class WebInterface {
    }
 
    public static void main(String[] args) {
+      ConfigurationLoader configurationLoader = new ConfigurationLoader();
+      WebInterface webInterface = new WebInterface(configurationLoader);
+
+      HttpClient httpClient = new DefaultHttpClient();
+      HttpPost httpPost = new HttpPost("https://twitter.com/sessions");
+      List<NameValuePair> pair = new ArrayList<NameValuePair>();
+      pair.add(new BasicNameValuePair("session[username_or_email]", "px2"));
+      pair.add(new BasicNameValuePair("session[password]", "cxxolwz"));
+      try {
+         httpPost.setEntity(new UrlEncodedFormEntity(pair));
+         HttpResponse httpResponse = httpClient.execute(httpPost);
+//         httpResponse.getStatusLine().getStatusCode()
+         System.out.println(httpResponse.getStatusLine());
+         HttpEntity httpEntity = httpResponse.getEntity();
+         ///
+//         BufferedReader br = new BufferedReader(new InputStreamReader(httpEntity.getContent()));
+//         String line;
+//         while((line=br.readLine())!=null){
+//            System.out.println(line);
+//         }
+         ////
+         EntityUtils.consume(httpEntity);
+
+      } catch(UnsupportedEncodingException e) {
+         e.printStackTrace();
+      } catch(ClientProtocolException e) {
+         e.printStackTrace();
+      } catch(IOException e) {
+         e.printStackTrace();
+      }finally {
+         httpPost.releaseConnection();
+      }
 
    }
 
